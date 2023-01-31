ee516baaa3624fe58015432691601fe06c8e19e9
Midnight-Knight <LTupov2003@yandex.ru>
2023-01-31 23:32:57 +0300

    Initial commit

---
+++ TS/script.ts
@@ -0,0 +1,128 @@
+let OperationArray:Array<string> = new Array(0);
+let bool:boolean = false;
+function Summation(x:number, y:number)
+{
+    return x + y;
+}
+
+function Subtraction(x:number, y:number)
+{
+    return x - y;
+}
+
+function Multiplication(x:number, y:number)
+{
+    return x * y;
+}
+
+function Division(x:number, y:number)
+{
+    return x / y;
+}
+
+function Remains(x:number, y:number)
+{
+    return x % y;
+}
+
+function Degree(x:number, y:number)
+{
+    return Math.pow(x,y);
+}
+
+function Root(x:number)
+{
+    return Math.sqrt(x);
+}
+
+function log(x:number)
+{
+    return Math.log(x);
+}
+
+function Add(x:string)
+{
+    if (OperationArray.length == 0)
+    {
+        if (x === '1' || x === '2' || x === '3' || x === '4' || x === '5' || x === '6' || x === '7' || x === '8' || x === '9')
+        {
+            OperationArray.push(x);
+        }
+    }
+    else if (x === '1' || x === '2' || x === '3' || x === '4' || x === '5' || x === '6' || x === '7' || x === '8' || x === '9' || x === '0' || x === ',')
+    {
+        if (OperationArray[OperationArray.length-1] === '1')
+        {
+            OperationArray[OperationArray.length - 1] += x;
+        }
+        else {
+
+        }
+    }
+    else
+    {
+        OperationArray.push(x);
+    }
+    var str:string = "";
+    for (var i:number = 0; i < OperationArray.length; ++i)
+    {
+        str += (" " + OperationArray[i]);
+    }
+    document.getElementById("Up").innerHTML = str;
+}
+
+function Del()
+{
+    OperationArray.pop();
+    var str:string = "";
+    for (var i:number = 0; i < OperationArray.length; ++i)
+    {
+        if (OperationArray[i].toString() === ',' || (i != 0 && ((OperationArray[i-1].toString() === '1' || OperationArray[i-1].toString() === '2' || OperationArray[i-1].toString() === '3' || OperationArray[i-1].toString() === '4' || OperationArray[i-1].toString() === '5' || OperationArray[i-1].toString() === '6' || OperationArray[i-1].toString() === '7' || OperationArray[i-1].toString() === '8' || OperationArray[i-1].toString() === '9' || OperationArray[i-1].toString() === '0' || OperationArray[i-1].toString() === ',') && (OperationArray[i].toString() === '1' || OperationArray[i].toString() === '2' || OperationArray[i].toString() === '3' || OperationArray[i].toString() === '4' || OperationArray[i].toString() === '5' || OperationArray[i].toString() === '6' || OperationArray[i].toString() === '7' || OperationArray[i].toString() === '8' || OperationArray[i].toString() === '9' || OperationArray[i].toString() === '0'))))
+        {
+            str += OperationArray[i].toString();
+        }
+        else
+        {
+            str += (" " + OperationArray[i].toString());
+        }
+    }
+    document.getElementById("Up").innerHTML = str;
+}
+
+function AllDel()
+{
+    OperationArray.length = 0;
+    document.getElementById('Up').innerHTML = "";
+}
+
+function clickMenu()
+{
+    var ArrClassFirst:NodeListOf<HTMLElement> = document.querySelectorAll('.First');
+    var ArrClassSecond:NodeListOf<HTMLElement> = document.querySelectorAll('.Second');
+    if (bool == false)
+    {
+        console.log('false');
+        for (var i:number = 0; i < ArrClassFirst.length; ++i)
+        {
+            ArrClassFirst[i].style.transform = "translate(0, -8vmin)";
+        }
+        for (var i:number = 0; i < ArrClassSecond.length; ++i)
+        {
+            ArrClassSecond[i].style.transform = "translate(0, -8vmin)";
+        }
+        bool = true;
+    }
+    else
+    {
+        console.log('true');
+        for (var i:number = 0; i < ArrClassFirst.length; ++i)
+        {
+            ArrClassFirst[i].style.transform = "translate(0, 0)";
+        }
+        for (var i:number = 0; i < ArrClassSecond.length; ++i)
+        {
+            ArrClassSecond[i].style.transform = "translate(0, 0)";
+        }
+        bool = false;
+    }
+}
